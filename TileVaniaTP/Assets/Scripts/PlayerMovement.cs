e589050e0e53da3f3fcba7f560d9ff0630df5056
TPearson420 <pearso420@morrisville.edu>
2023-12-03 01:04:21 -0500

    Jummp

--- TileVaniaTP/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaTP/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
@@ -29,18 +30,28 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
+
+    void OnJump(InputValue value)
+    {
+
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run() 
     {
 
         Vector2 playerVelocity = new Vector2(moveInput.x *runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) < Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("IsRunning" , playerHasHorizontalSpeed);
     }
     void FlipSprite() 
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) < Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed)
         {
 
