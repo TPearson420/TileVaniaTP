d8bc549185033a5d5c51fc3fd3a91dafec45dbe1
TPearson420 <pearso420@morrisville.edu>
2023-12-03 01:19:06 -0500

    Jump if touching

--- TileVaniaTP/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaTP/Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,12 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
    
@@ -33,7 +35,8 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) 
+        { return; }
         if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
