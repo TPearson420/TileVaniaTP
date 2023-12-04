dad69bf4d3c42b689acef9df5c7a6f25666fb52e
TPearson420 <pearso420@morrisville.edu>
2023-12-03 20:34:38 -0500

    Player Mortality

--- TileVaniaTP/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaTP/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeetCollider;
     float gravityScaleAtStart;
+
+    bool isAlive = true;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
@@ -28,20 +30,33 @@ public class PlayerMovement : MonoBehaviour
    
     void Update()
     {
+        if (!isAlive)
+        {
+            return;
+        }
         Run();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
 
      void OnMove(InputValue value) 
-    { 
+    {
+        if (!isAlive)
+        {
+            return;
+        }
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
 
     void OnJump(InputValue value)
     {
-        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) 
+        if (!isAlive)
+        {
+            return;
+        }
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) 
         { return; }
         if (value.isPressed)
         {
@@ -82,5 +97,12 @@ public class PlayerMovement : MonoBehaviour
        
         bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
         myAnimator.SetBool("IsClimbing" , playerHasVerticalSpeed);
+    }
+    void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        {
+            isAlive = false;
+        }
     }
 }
